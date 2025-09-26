e953645929f647871cf5069b6a29fef194f2f904
Kevin Dalton <kmdalton@slac.stanford.edu>
2025-09-26 11:07:13 -0700

    add abismal setup script template

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#EDIT setup script location:
+#Script provides abismal and PHENIX
+source ~/tools/abismal/setup.sh
+
+# Handle ./setup.sh and source setup.sh
+[[ $0 != $BASH_SOURCE ]] && 
+	ABISMALDIR=`dirname $BASH_SOURCE` ||
+	ABISMALDIR=`dirname $0` 
+
+export ABISMALDIR=`readlink -f $ABISMALDIR`
+echo "setting..." 
+echo "    ABISMALDIR=$ABISMALDIR"
+
